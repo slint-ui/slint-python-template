a2cdddb14b06ad36a43aa339ae41a40f944df805
Simon Hausmann <hausmann@gmail.com>
2025-03-25 08:16:39 +0100

    Simplify Slint file loader
    We don't need to mess with sys.path :)

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import slint
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "ui"))
 
-class MainWindow(slint.loader.app_window.AppWindow):
+class MainWindow(slint.loader.ui.app_window.AppWindow):
     @slint.callback
     def request_increase_value(self):
         self.counter = self.counter + 1
+
 
 main_window = MainWindow()
 main_window.show()
